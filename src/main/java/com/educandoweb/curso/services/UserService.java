942467f393e99c758a16c94240d759ad11142e87
DanilodeRamos <116719740+DanilodRamos@users.noreply.github.com>
2026-09-18 19:07:34 -0300

    User Update

--- src/main/java/com/educandoweb/curso/services/UserService.java
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -5,35 +5,57 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.educandoweb.curso.entities.User;
 import com.educandoweb.curso.repositories.UserRepository;
 
-//registrando com anotation
+// Registrando com annotation
 @Service
 public class UserService {
-	// operaçao pra buscar todos usuarios e id
-	// injeçaõ de dependencia
-	@Autowired
-	private UserRepository repository;
 
-	public List<User> findAll() {
-		return repository.findAll();
+    // Operação para buscar todos usuários e id
+    // Injeção de dependência
+    @Autowired
+    private UserRepository repository;
 
-	}
+    public List<User> findAll() {
+        return repository.findAll();
+    }
 
-	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id);
-		return obj.get();
-	}
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
 
-	public User insert(User obj) {//operaçao basica pra inserir um novo usuario
-		return repository.save(obj);
-	}
-	
-	
-	//metodo para deletar
-	public void delete(Long id) {
-		repository.deleteById(id);
-	}
+    public User insert(User obj) {
+        // Operação básica para inserir um novo usuário
+        return repository.save(obj);
+    }
+
+    // Método para deletar
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    // Método para atualizar usuário
+    @Transactional
+    public User update(Long id, User obj) {
+
+        // Busca a referência do usuário pelo ID
+        User entity = repository.getReferenceById(id);
+
+        // Atualiza os dados
+        updateData(entity, obj);
+
+        // Salva a alteração
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
