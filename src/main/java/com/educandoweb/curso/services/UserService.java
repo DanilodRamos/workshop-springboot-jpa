1a53108c2682f63d1aaeb61d5b49d8cf1401c765
DanilodeRamos <116719740+DanilodRamos@users.noreply.github.com>
2026-09-24 18:55:45 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/curso/services/UserService.java
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -15,65 +15,70 @@ import com.educandoweb.curso.resources.exceptions.ResourceExceptionHandler;
 import com.educandoweb.curso.services.exceptions.DatabaseException;
 import com.educandoweb.curso.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 // Registrando com annotation
 @Service
 public class UserService {
 
-    private final ResourceExceptionHandler resourceExceptionHandler;
+	private final ResourceExceptionHandler resourceExceptionHandler;
 	// Operação para buscar todos usuários e id
-    // Injeção de dependência
-    @Autowired
-    private UserRepository repository;
+	// Injeção de dependência
+	@Autowired
+	private UserRepository repository;
 
 	UserService(ResourceExceptionHandler resourceExceptionHandler) {
 		this.resourceExceptionHandler = resourceExceptionHandler;
 	}
 
-    public List<User> findAll() {
-        return repository.findAll();
-    }
+	public List<User> findAll() {
+		return repository.findAll();
+	}
 
-    public User findById(Long id) {
-        Optional<User> obj = repository.findById(id);
-        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-    }
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
 
-    public User insert(User obj) {
-        // Operação básica para inserir um novo usuário
-        return repository.save(obj);
-    }
+	public User insert(User obj) {
+		// Operação básica para inserir um novo usuário
+		return repository.save(obj);
+	}
 
-    // Método para deletar
-    public void delete(Long id) {
-    	try{
-    		 repository.deleteById(id);
-    	}
-       catch(EmptyResultDataAccessException e) {
-    	   e.printStackTrace();
-    	   throw new ResourceNotFoundException(id);
-       } catch (DataIntegrityViolationException e) {
-    	   throw new DatabaseException(e.getMessage());//lancando excessao da minha camada de servico
-       }
-    }
+	// Método para deletar
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());// lancando excessao da minha camada de servico
+		}
+	}
 
-    // Método para atualizar usuário
-    @Transactional
-    public User update(Long id, User obj) {
+	// Método para atualizar usuário
+	@Transactional
+	public User update(Long id, User obj) {
 
-        // Busca a referência do usuário pelo ID
-        User entity = repository.getReferenceById(id);
+		// Busca a referência do usuário pelo ID
+		try {
+			User entity = repository.getReferenceById(id);
 
-        // Atualiza os dados
-        updateData(entity, obj);
+			// Atualiza os dados
+			updateData(entity, obj);
 
-        // Salva a alteração
-        return repository.save(entity);
-    }
+			// Salva a alteração
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
 
-    private void updateData(User entity, User obj) {
+	private void updateData(User entity, User obj) {
 
-        entity.setName(obj.getName());
-        entity.setEmail(obj.getEmail());
-        entity.setPhone(obj.getPhone());
-    }
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
